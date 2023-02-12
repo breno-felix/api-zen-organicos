e03333f5d09c00629de3c7076cdeb1ad10cc1162
Breno Felix <brenodev.felix@gmail.com>
2023-02-11 22:57:55 -0300

    :lock: Create auth admin Middleware to verify admin user.

--- src/routes/product/ProductRoute.js
+++ src/routes/product/ProductRoute.js
@@ -1,9 +1,10 @@
 const route = require('express').Router()
 const ProductController = require('../../controllers/product/ProductController')
 const authMiddleware = require('../../middlewares/auth')
+const authAdminMiddleware = require('../../middlewares/authAdmin')
 
 route.use(authMiddleware)
-route.post('/new-product', ProductController.store)
+route.post('/new-product', authAdminMiddleware, ProductController.store)
 route.get('/index-product', ProductController.index)
 
 module.exports = route
