369f9514db6aa758544fc5a247865978b753c4b1
Breno Felix <brenodev.felix@gmail.com>
2023-02-12 18:34:36 -0300

    :sparkles: feat: Create upload order route

--- src/routes/order/OrderRoute.js
+++ src/routes/order/OrderRoute.js
@@ -1,7 +1,15 @@
 const route = require('express').Router()
 const OrderController = require('../../controllers/order/OrderController')
+const OrderUploadController = require('../../controllers/order/OrderUploadController')
 const authMiddleware = require('../../middlewares/auth')
+const multerMiddleware = require('../../middlewares/multer')
 
 route.post('/new-order', authMiddleware, OrderController.store)
+route.post(
+  '/upload-order',
+  authMiddleware,
+  multerMiddleware,
+  OrderUploadController.upload
+)
 
 module.exports = route
--- src/services/product/ProductService.js
+++ src/services/product/ProductService.js
@@ -3,9 +3,11 @@ const Product = require('../../models/Product')
 const create = (body) => Product.create(body)
 const loadAll = () => Product.find()
 const loadById = (id) => Product.findById(id)
+const loadByName = (name) => Product.findOne({ name })
 
 module.exports = {
   create,
   loadAll,
-  loadById
+  loadById,
+  loadByName
 }
